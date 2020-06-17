93ff9ffbe72c0f46220e92da033723534a376003
felipemaxplay <66925822+felipemaxplay@users.noreply.github.com>
2020-06-16 21:25:29 -0300

    v0.1.3
    Já add um pouco de javascript

--- js/code.js
+++ js/code.js
@@ -1,30 +1,70 @@
-var flag_jogado_jog1;
-var flag_jogado_jog2;
+var flag_jogando_jog2;
+var flag_jogando_jog1;
 var dado1;
 var dado2;
 
-function getRandomInt(min, max) {
-  return Match.floor(Match.random() * (max - min + 1)) + min;
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function sorteia1() {
-  //sorteia um numero de 1 a 6
-  valor_dado1=getRandomInt(1, 6);
-  //trocar imagem do dados
-  if(valor_dado1==1) document.getElementById('img1').src="dado_1.png";
-    else if (valor_dado1==2) document.getElementById('img1').src="dado_2.png";
-     else if (valor_dado1==3) document.getElementById('img1').src="dado_3.png";
-      else if (valor_dado1==4) document.getElementById('img1').src="dado_4.png";
-        else if (valor_dado1==5) document.getElementById('img1').src="dado_5.png";
-          else document.getElementById('img1').src="dado_6.png";
-};
+function sorteia1 () {
+//dado do 1 até o 6
+valor_dado = getRandomInt(1, 6);
 
-var imgMemo;
+switch (valor_dado) {
 
-function Grande(imagem) {
-  // imgMemo = document.getElementById("img1").src;
+  case 1:
+    document.getElementById('img1').src = 'imagens/dado_1.png';
+    break;
+  case 2:
+    document.getElementById('img1').src = 'imagens/dado_2.png';
+    break;
+  case 3:
+    document.getElementById('img1').src = 'imagens/dado_3.png';
+    break;
+  case 4:
+    document.getElementById('img1').src = 'imagens/dado_4.png';
+    break;
+  case 5:
+    document.getElementById('img1').src = 'imagens/dado_5.png';
+    break;
+  case 6:
+    document.getElementById('img1').src = 'imagens/dado_6.png';
+    break;
 
-    document.getElementById("img1").src = imagem.src;
+  default:
 
-  //  imagem.src = imgMemo;
+}
+
+}
+
+function sorteia2 () {
+//dado do 1 até o 6
+valor_dado = getRandomInt(1, 6);
+
+switch (valor_dado) {
+
+  case 1:
+    document.getElementById('img2').src = 'imagens/dado_1.png';
+    break;
+  case 2:
+    document.getElementById('img2').src = 'imagens/dado_2.png';
+    break;
+  case 3:
+    document.getElementById('img2').src = 'imagens/dado_3.png';
+    break;
+  case 4:
+    document.getElementById('img2').src = 'imagens/dado_4.png';
+    break;
+  case 5:
+    document.getElementById('img2').src = 'imagens/dado_5.png';
+    break;
+  case 6:
+    document.getElementById('img2').src = 'imagens/dado_6.png';
+    break;
+
+  default:
+
+}
+
 }
