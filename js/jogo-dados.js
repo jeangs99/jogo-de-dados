49b4a0b0c3e4245598fec55928029f641aec37de
felipemaxplay <66925822+felipemaxplay@users.noreply.github.com>
2020-06-19 17:20:20 -0300

    V1.0

---
+++ js/jogo-dados.js
@@ -0,0 +1,11 @@
+function cadastraJogador (btnEnvia){
+	var nome;
+	var nick;
+	var idade;
+	/*document.querySelector('#btnEnvia').addEventListener('click', () => {
+		window.location.href = 'jogo-dados.html';
+	});*/
+
+	//alert('Funcionando');
+
+}
